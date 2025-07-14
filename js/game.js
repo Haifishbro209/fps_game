36d7b047b37de9ec0d0ee49c233716609c796469
haifishbro209 <kaloyannikolov016@gmail.com>
2025-07-14 19:44:41 +0200

    wasd

--- js/game.js
+++ js/game.js
@@ -336,10 +336,10 @@ function onKeyDown(event) {
     if (!gameStarted) return;
     
     switch (event.code) {
-        case 'KeyW':
+        case 'KeyS':
             moveForward = true;
             break;
-        case 'KeyS':
+        case 'KeyW':
             moveBackward = true;
             break;
         case 'KeyA':
@@ -358,10 +358,10 @@ function onKeyDown(event) {
 
 function onKeyUp(event) {
     switch (event.code) {
-        case 'KeyW':
+        case 'KeyS':
             moveForward = false;
             break;
-        case 'KeyS':
+        case 'KeyW':
             moveBackward = false;
             break;
         case 'KeyA':
